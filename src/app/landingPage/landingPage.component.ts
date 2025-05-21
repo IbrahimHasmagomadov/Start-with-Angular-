dea519975f418be4693441ca98404248289f6693
IbrahimHasmagomadov <Hasmagomadov@googlemail.com>
2025-05-21 11:11:41 +0200

    start with Srakura

---
+++ src/app/landingPage/landingPage.component.ts
@@ -0,0 +1,17 @@
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterOutlet } from "@angular/router";
+
+@Component({
+    selector: 'app-landingPage',
+    standalone: true,
+    imports: [CommonModule, RouterOutlet, LandingPageComponent],
+    template: /*html*/`
+        <section>
+            <h1>SAKURA RAMEN</h1>
+            <h2>BEST RAMEN IN TOWN</h2>
+        </section>
+    `,
+    styleUrls: ['./landingPage.component.scss']
+})
+export class LandingPageComponent {}
